df8a61dcd486cdac7d86085e4831f56b726be6a0
Gijs Landwehr <gijslandwehr@utexas.edu>
2024-09-07 05:19:08 -0500

    reformat sysid routines

--- src/main/java/frc/robot/sysid/SysIdRoutines.java
+++ src/main/java/frc/robot/sysid/SysIdRoutines.java
@@ -6,47 +6,59 @@ import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.Arm;
 
 public class SysIdRoutines {
-    private final SwerveRequest.SysIdSwerveTranslation SysIDTranslate = new SwerveRequest.SysIdSwerveTranslation();
+        public static final SwerveRequest.SysIdSwerveTranslation SysIDTranslate = new SwerveRequest.SysIdSwerveTranslation();
 
-    @SuppressWarnings("unused")
-    private SysIdRoutine translateRoutine = new SysIdRoutine(
-            new SysIdRoutine.Config(
-                    null,
-                    Units.Volts.of(4),
-                    null,
-                    (state) -> SignalLogger.writeString("state", state.toString())),
-            new SysIdRoutine.Mechanism(
-                    (volts) -> TunerConstants.DriveTrain.setControl(SysIDTranslate.withVolts(volts)),
-                    null,
-                    TunerConstants.DriveTrain));
+        public static final SysIdRoutine translateRoutine = new SysIdRoutine(
+                        new SysIdRoutine.Config(
+                                        null,
+                                        Units.Volts.of(4),
+                                        null,
+                                        (state) -> SignalLogger.writeString("state", state.toString())),
+                        new SysIdRoutine.Mechanism(
+                                        (volts) -> TunerConstants.DriveTrain
+                                                        .setControl(SysIDTranslate.withVolts(volts)),
+                                        null,
+                                        TunerConstants.DriveTrain));
 
-    private final SwerveRequest.SysIdSwerveRotation SysIDRotate = new SwerveRequest.SysIdSwerveRotation();
+        private static final SwerveRequest.SysIdSwerveRotation SysIDRotate = new SwerveRequest.SysIdSwerveRotation();
 
-    @SuppressWarnings("unused")
-    private SysIdRoutine rotateRoutine = new SysIdRoutine(
-            new SysIdRoutine.Config(
-                    null,
-                    Units.Volts.of(4),
-                    null,
-                    (state) -> SignalLogger.writeString("state", state.toString())),
-            new SysIdRoutine.Mechanism(
-                    (volts) -> TunerConstants.DriveTrain.setControl(SysIDRotate.withVolts(volts)),
-                    null,
-                    TunerConstants.DriveTrain));
+        public static final SysIdRoutine rotateRoutine = new SysIdRoutine(
+                        new SysIdRoutine.Config(
+                                        null,
+                                        Units.Volts.of(4),
+                                        null,
+                                        (state) -> SignalLogger.writeString("state", state.toString())),
+                        new SysIdRoutine.Mechanism(
+                                        (volts) -> TunerConstants.DriveTrain.setControl(SysIDRotate.withVolts(volts)),
+                                        null,
+                                        TunerConstants.DriveTrain));
 
-    private final SwerveRequest.SysIdSwerveSteerGains SysIDSteer = new SwerveRequest.SysIdSwerveSteerGains();
+        private static final SwerveRequest.SysIdSwerveSteerGains SysIDSteer = new SwerveRequest.SysIdSwerveSteerGains();
 
-    @SuppressWarnings("unused")
-    private SysIdRoutine steerRoutine = new SysIdRoutine(
-            new SysIdRoutine.Config(
-                    null,
-                    Units.Volts.of(4),
-                    null,
-                    (state) -> SignalLogger.writeString("state", state.toString())),
-            new SysIdRoutine.Mechanism(
-                    (volts) -> TunerConstants.DriveTrain.setControl(SysIDSteer.withVolts(volts)),
-                    null,
-                    TunerConstants.DriveTrain));
+        public static final SysIdRoutine steerRoutine = new SysIdRoutine(
+                        new SysIdRoutine.Config(
+                                        null,
+                                        Units.Volts.of(4),
+                                        null,
+                                        (state) -> SignalLogger.writeString("state", state.toString())),
+                        new SysIdRoutine.Mechanism(
+                                        (volts) -> TunerConstants.DriveTrain.setControl(SysIDSteer.withVolts(volts)),
+                                        null,
+                                        TunerConstants.DriveTrain));
+
+        // pretend to output with volts even though is amps
+        public static final SysIdRoutine armTorqueCurrentRoutine = new SysIdRoutine(
+                        new SysIdRoutine.Config(
+                                        Units.Volts.of(0.1).per(Units.Seconds.of(1)),
+                                        Units.Volts.of(2.0),
+                                        null,
+                                        (state) -> SignalLogger.writeString("state", state.toString())),
+                        new SysIdRoutine.Mechanism(
+                                        (amps) -> Arm.getInstance().setAmpsSysID(amps.magnitude()),
+                                        null,
+                                        Arm.getInstance()));
+
 }
